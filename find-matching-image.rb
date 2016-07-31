7eebc042a643a7cdc236e0223097e39f56d94b7e
Andrew Matheny <andrew.j.matheny@gmail.com>
2016-07-31 10:47:41 -0400

    Initial WIP commit

---
+++ find-matching-image.rb
@@ -0,0 +1,207 @@
+#!/usr/bin/env ruby
+
+require 'zip'
+require 'string/similarity'
+require 'nokogiri'
+require 'json'
+
+vassal_mod = ARGV[0]
+
+def extract_zip(file, destination)
+  FileUtils.mkdir_p(destination)
+
+  Zip::File.open(file) do |zip_file|
+    zip_file.each do |f|
+      fpath = File.join(destination, f.name)
+      zip_file.extract(f, fpath) unless File.exist?(fpath)
+    end
+  end
+end
+
+def load_card_images()
+  cards = []
+  for f in Dir.glob("#{File.dirname(__FILE__)}/images/**/*") do
+    next if !f.match(/.*.jpg$/) and !f.match(/.*.png$/)
+    split = f.split('/')
+    card = {}
+    card[:type] = split[2]
+    card[:path] = f
+    if card[:type] == "pilots"
+      card[:faction] = split[3]
+      card[:ship] = split[4]
+      card[:name] = split[5].gsub(".png", "").gsub(".jpg", "")
+    else
+      card[:upgrade_type] = split[3]
+      card[:name] = split[4].gsub(".png", "").gsub(".jpg", "")
+    end
+    cards << card
+  end
+  cards
+end
+
+def load_vassal_cards(zip_dir)
+  build_file = Nokogiri::XML(File.open("#{zip_dir}/buildFile"))
+
+  vassal_cards = []
+
+  for piece in build_file.xpath("//VASSAL.build.widget.PieceSlot") do
+    card = {}
+    next if piece.parent["entryName"] == "Images for Cards"
+    if piece.content.include?("Pilot Card")
+      card[:type] = "pilots"
+      card[:ship] = piece.parent["entryName"]
+      card[:faction] = piece.parent.parent["entryName"]
+      card[:name] = piece["entryName"]
+      card[:path] = piece.content.split(';')[12]
+      vassal_cards << card
+    elsif piece.content.include?("Upgrades")
+      card[:type] = "upgrades"
+      card[:upgrade_type] = piece.parent["entryName"]
+      card[:name] = piece["entryName"]
+      card[:path] = piece.content.split(';')[16]
+      vassal_cards << card
+    end
+  end
+  vassal_cards
+end
+
+def find_match(cards, name, type, faction = nil, ship = nil)
+  candidates = cards.select { |c| c[:type] == type }
+  candidates = cards.select { |c| c[:faction] == faction } if faction
+  candidates = cards.select { |c| c[:ship] == ship } if ship
+  result = { match: nil, score: -1 }
+  for candidate in candidates do
+    score = String::Similarity.cosine(candidate[:name], name)
+    result = { match: candidate, score: score } if score > result[:score]
+  end
+  result
+end
+
+def normalize_vassal_name(f)
+  f.gsub("_", "").gsub("-", "").gsub(" ", "").gsub("/", "").gsub("'", "").gsub('.', "").gsub('"', "").downcase
+end
+
+def is_excluded(name)
+  ["firstorderback", "resistanceback", "back",
+    "imperialback", "rebelback", "scumback"].include? name or name.match(/.*crippled$/)
+end
+
+def get_faction(vassal_pilot)
+  if vassal_pilot[:faction].match(/.*scum.*/i)
+    "scum"
+  elsif vassal_pilot[:faction].match(/.*imperial.*/i)
+    "imperial"
+  elsif vassal_pilot[:faction].match(/.*rebel.*/i)
+    "rebels"
+  else
+    nil
+  end
+end
+
+def match(target, candidates)
+  result = { match: nil, score: -1 }
+  for candidate in candidates
+    score = String::Similarity.cosine(candidate, target)
+    result = { match: candidate, score: score } if score > result[:score]
+  end
+  result
+end
+
+def match_ship(vassal_pilot, faction, cards, overrides)
+  vassal_ship_name = normalize_vassal_name(vassal_pilot[:ship])
+  if overrides["ship"][vassal_ship_name]
+    return { match: overrides["ship"][vassal_ship_name].to_s, score: 100 }
+  end
+  ships = cards.select { |c| c[:faction] == faction }.collect { |c| c[:ship] }.uniq
+  match(vassal_ship_name, ships)
+end
+
+def match_upgrade_type(vassal_upgrade, cards, overrides)
+  vassal_upgrade_type = normalize_vassal_name(vassal_upgrade[:upgrade_type])
+  if overrides["upgrade_type"][vassal_upgrade_type]
+    return { match: overrides["upgrade_type"][vassal_upgrade_type].to_s, score: 100 }
+  end
+  upgrade_types = cards.select { |c| c[:type] == "upgrades" }.collect {|c| c[:upgrade_type]}.uniq
+  match(vassal_upgrade_type, upgrade_types)
+end
+
+def match_upgrade_card(vassal_upgrade, cards, overrides)
+  upgrade_type = match_upgrade_type(vassal_upgrade, cards, overrides)[:match]
+  candidates = cards.select { |c| c[:upgrade_type] == upgrade_type }
+  names = candidates.collect{|c| c[:name] }.uniq
+  match = match(normalize_vassal_name(vassal_upgrade[:name]), names)
+  {
+    match: candidates.select { |c| c[:name] == match[:match] }[0],
+    score: match[:score]
+  }
+end
+
+def match_pilot_card(vassal_pilot, cards, overrides)
+  faction = get_faction(vassal_pilot)
+  ship = match_ship(vassal_pilot, faction, cards, overrides)[:match]
+  candidates = cards.select { |c| c[:faction] == faction and c[:ship] == ship}
+  names = candidates.collect { |c| c[:name] }.uniq
+  match = match(normalize_vassal_name(vassal_pilot[:name]), names)
+  {
+    match: candidates.select { |c| c[:name] == match[:match] }[0],
+    score: match[:score]
+  }
+end
+
+def card_to_s(card)
+  if card[:type] == "pilots"
+    "#{card[:faction]}/#{card[:ship]}/#{card[:name]}"
+  else
+    "#{card[:upgrade_type]}/#{card[:name]}"
+  end
+end
+
+#######################################
+
+overrides = JSON.parse(File.read("#{File.dirname(__FILE__)}/overrides.json"))
+
+zip_dir = Dir.mktmpdir("#{File.basename(vassal_mod)}.dir")
+extract_zip(vassal_mod, zip_dir)
+
+cards = load_card_images
+vassal_cards = load_vassal_cards(zip_dir)
+
+for vassal_card in vassal_cards do
+  match = nil
+  if vassal_card[:type] == "pilots"
+    match = match_pilot_card(vassal_card, cards, overrides)
+  end
+
+  if vassal_card[:type] == "upgrades"
+    match = match_upgrade_card(vassal_card, cards, overrides)
+  end
+  if match[:score] < 1.0
+    puts "#{match[:score]}: #{card_to_s(vassal_card)} -> #{card_to_s(match[:match])}"
+  end
+end
+
+
+# for pilot_img in Dir.glob("#{zip_dir}/images/Pilot-*") do
+#   name = normalize_vassal_file_name pilot_img
+#   if is_excluded(name)
+#     next
+#   end
+#
+#   faction = nil
+#   ship = nil
+#   if name.include?("s&v") or name.include?("scum")
+#     faction = "scum"
+#     name = name.gsub("s&v", "").gsub("scum", "")
+#   end
+#   if name.include?("shuttle")
+#     ship = "attackshuttle"
+#     name = name.gsub("shuttle", "")
+#   end
+#
+#   match = find_match(cards, name, "pilots", faction, ship)
+#   if match[:score] < 1.0
+#     puts "#{name} => #{match[:score]} #{match[:match][:path]}"
+#   end
+# end
+
+puts "zip_dir is #{zip_dir}"
